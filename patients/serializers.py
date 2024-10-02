9e505e6a52ad42cd948dcdd8fd8848cfac793e15
Jesus Vergara <104742114+JesusVergara89@users.noreply.github.com>
2024-10-02 19:30:24 +0000

    adding age to the patients

--- patients/serializers.py
+++ patients/serializers.py
@@ -1,21 +1,29 @@
+from datetime import date
 from rest_framework import serializers
 from .models import Patient, Insurance, MedicalRecord
 from bookings.serializers import AppointmentSerializer
 
+
 class PatientSerializer(serializers.ModelSerializer):
     appointments = AppointmentSerializer(many=True, read_only=True)
+    age ) serializers.SerializerMethodField()
     class Meta:
         model = Patient
         fields = [
             "id",
             "first_name",
             "date_of_bith",
+            "age",
             "contact_number",
             "email",
             "address",
             "medical_history",
             "appointments"
         ]
+    def get_age(self, obj):
+        age_td = date.today() - obj.date_of_bith
+        return age_td // 365
+
 
 class InsuranceSerializer(serializers.ModelSerializer):
     class Meta:
